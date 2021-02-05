5d9b89e36f55f1cd55e9b42dce71424d20554a90
lorusak <loup.rusak@etu.uca.fr>
2021-02-05 16:09:02 +0100

    modifs

--- NexusDungeon.Android/Activity1.cs
+++ NexusDungeon.Android/Activity1.cs
@@ -3,6 +3,7 @@ using Android.Content.PM;
 using Android.OS;
 using Android.Views;
 using Microsoft.Xna.Framework;
+using NexusDungeon.Core;
 
 namespace NexusDungeon.Android
 {
@@ -17,14 +18,14 @@ namespace NexusDungeon.Android
     )]
     public class Activity1 : AndroidGameActivity
     {
-        private Game1 _game;
+        private NexusDungeonGame _game;
         private View _view;
 
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
-            _game = new Game1();
+            _game = new NexusDungeonGame();
             _view = _game.Services.GetService(typeof(View)) as View;
 
             SetContentView(_view);
